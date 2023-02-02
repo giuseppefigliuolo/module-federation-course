bb0c8a1d2700f48b27debe37a418f9b376151757
Giuseppe Figliuolo <giuseppe.figliuolo@bitrock.it>
2023-02-02 14:52:25 +0100

    init

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,29 @@
+//merge serve a mergiare più config tra loro
+const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const commonConfig = require('./webpack.common')
+
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        // 'marketing@ matches con il nome dato al remote nella config
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js'
+      }
+    })
+  ]
+}
+// il secondo argomento ha la precedenza sulla prima configurazione (commonconfig) e riscrive le parti di configurazione in comune
+module.exports = merge(commonConfig, devConfig)
