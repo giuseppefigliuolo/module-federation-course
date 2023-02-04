f01dea8a41117cc9da7b61884ff68b6f1920e0b4
Giuseppe Figliuolo <giuseppe.figliuolo@bitrock.it>
2023-02-04 12:45:08 +0100

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from '../components/MarketingApp'
+import Header from '../components/Header'
 
 export default () => {
   return (
-    <div>
-      <p>Ciaoooo</p>
-      <p>Ciaoooo</p>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   )
 }
--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -72,7 +72,6 @@ export default function Album() {
         {/* Hero unit */}
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
-            <p>CIAOOONE</p>
             <Typography
               component="h1"
               variant="h2"
